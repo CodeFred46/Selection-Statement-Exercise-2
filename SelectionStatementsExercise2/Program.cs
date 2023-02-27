d7324bb79bd8c82ab261ceada4577521cd8cb4a8
fred47 <Frdrck.hart@gmail.com>
2023-02-27 06:08:01 -0500

    Finished Selection Statements 2 Exercise

--- SelectionStatementsExercise2/Program.cs
+++ SelectionStatementsExercise2/Program.cs
@@ -4,7 +4,32 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("What is your favorite school subject?");
+
+            var subject = Console.ReadLine();
+
+            switch (subject)
+            {
+
+                case "Math":
+                    Console.WriteLine("Math is hard");
+                    break;
+                case "Science":
+                    Console.WriteLine("Science is fun");
+                    break;
+                case "English":
+                    Console.WriteLine("English is the best");
+                    break;
+                case "Social Studies":
+                    Console.WriteLine("Social Studies is amazing");
+                    break;
+                default:
+                    Console.WriteLine($"I never heard of {subject}");
+                    break;
+            }
+
+
+
+
         }
-    }
-}
+    }    }    
